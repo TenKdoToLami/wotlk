1f8a02d12407d2eeac1cbf8beb21f5ea68d0d61d
TenKdoToLami <honzajanbartos@seznam.cz>
2025-08-21 17:57:31 +0200

    Formating

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const createParticle = () => {
-    const particle = document.createElement("div");
-    particle.classList.add("frost-particle");
-    particle.style.left = Math.random() * 100 + "vw";
-    particle.style.animationDuration = 8 + Math.random() * 5 + "s";
-    document.body.appendChild(particle);
+	const createParticle = () => {
+		const particle = document.createElement("div");
+		particle.classList.add("frost-particle");
+		particle.style.left = Math.random() * 100 + "vw";
+		particle.style.animationDuration = 8 + Math.random() * 5 + "s";
+		document.body.appendChild(particle);
 
-    setTimeout(() => particle.remove(), 15000);
-  };
+		setTimeout(() => particle.remove(), 15000);
+	};
 
-  setInterval(createParticle, 60);
+	setInterval(createParticle, 60);
 });
